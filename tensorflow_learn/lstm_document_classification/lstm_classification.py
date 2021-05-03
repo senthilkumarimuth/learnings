09e2ca23160ca64f64418e790f9a39caa02cdc76
suganthi <suganthimsc3@gmail.com>
2021-05-03 20:45:22 +0530

    lstm final

---
+++ tensorflow_learn/lstm_document_classification/lstm_classification.py
@@ -0,0 +1,125 @@
+import csv
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from nltk.corpus import stopwords
+STOPWORDS = set(stopwords.words('english'))
+
+print(tf.__version__)
+
+vocab_size = 5000
+embedding_dim = 64
+max_length = 200
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = '<OOV>'
+training_portion = .8
+
+articles = []
+labels = []
+
+with open("bbc-text.csv", 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    next(reader)
+    for row in reader:
+        labels.append(row[0])
+        article = row[1]
+        for word in STOPWORDS:
+            token = ' ' + word + ' '
+            article = article.replace(token, ' ')
+            article = article.replace(' ', ' ')
+        articles.append(article)
+print(len(labels))
+print(len(articles))
+
+
+train_size = int(len(articles) * training_portion)
+
+train_articles = articles[0: train_size]
+train_labels = labels[0: train_size]
+
+validation_articles = articles[train_size:]
+validation_labels = labels[train_size:]
+
+print(train_size)
+print(len(train_articles))
+print(len(train_labels))
+print(len(validation_articles))
+print(len(validation_labels))
+
+train_padded = pad_sequences(train_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+print(len(train_sequences[0]))
+print(len(train_padded[0]))
+
+print(len(train_sequences[1]))
+print(len(train_padded[1]))
+
+print(len(train_sequences[10]))
+print(len(train_padded[10]))
+
+validation_sequences = tokenizer.texts_to_sequences(validation_articles)
+validation_padded = pad_sequences(validation_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+print(len(validation_sequences))
+print(validation_padded.shape)
+
+label_tokenizer = Tokenizer()
+label_tokenizer.fit_on_texts(labels)
+
+training_label_seq = np.array(label_tokenizer.texts_to_sequences(train_labels))
+validation_label_seq = np.array(label_tokenizer.texts_to_sequences(validation_labels))
+print(training_label_seq[0])
+print(training_label_seq[1])
+print(training_label_seq[2])
+print(training_label_seq.shape)
+
+print(validation_label_seq[0])
+print(validation_label_seq[1])
+print(validation_label_seq[2])
+print(validation_label_seq.shape)
+
+
+
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+def decode_article(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+print(decode_article(train_padded[10]))
+print('---')
+print(train_articles[10])
+
+model = tf.keras.Sequential([
+    # Add an Embedding layer expecting input vocab of size 5000, and output embedding dimension of size 64 we set at the top
+    tf.keras.layers.Embedding(vocab_size, embedding_dim),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(embedding_dim)),
+#    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
+    # use ReLU in place of tanh function since they are very good alternatives of each other.
+    tf.keras.layers.Dense(embedding_dim, activation='relu'),
+    # Add a Dense layer with 6 units and softmax activation.
+    # When we have multiple outputs, softmax convert outputs layers into a probability distribution.
+    tf.keras.layers.Dense(6, activation='softmax')
+])
+model.summary()
+
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+num_epochs = 10
+history = model.fit(train_padded, training_label_seq, epochs=num_epochs, validation_data=(validation_padded, validation_label_seq), verbose=2)
+
+
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+  
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
+
+
+
+
+
