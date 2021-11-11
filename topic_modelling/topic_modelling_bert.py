d017e4a161e2f2b362ba1513c0b070779f331c11
senthil.marimuthu <senthil.marimuthu@htcindia.com>
2021-11-11 19:29:12 +0530

    bertopic is updated

--- topic_modelling/topic_modelling_bert.py
+++ topic_modelling/topic_modelling_bert.py
@@ -4,3 +4,104 @@ data = fetch_20newsgroups(subset='all')['data']
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('distilbert-base-nli-mean-tokens')
 embeddings = model.encode(data, show_progress_bar=True)
+
+import umap
+umap_embeddings = umap.UMAP(n_neighbors=15,
+                            n_components=5,
+                            metric='cosine').fit_transform(embeddings)
+
+import hdbscan
+cluster = hdbscan.HDBSCAN(min_cluster_size=15,
+                          metric='euclidean',
+                          cluster_selection_method='eom').fit(umap_embeddings)
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+# Prepare data
+umap_data = umap.UMAP(n_neighbors=15, n_components=2, min_dist=0.0, metric='cosine').fit_transform(embeddings)
+result = pd.DataFrame(umap_data, columns=['x', 'y'])
+result['labels'] = cluster.labels_
+
+# Visualize clusters
+fig, ax = plt.subplots(figsize=(20, 10))
+outliers = result.loc[result.labels == -1, :]
+clustered = result.loc[result.labels != -1, :]
+plt.scatter(outliers.x, outliers.y, color='#BDBDBD', s=0.05)
+plt.scatter(clustered.x, clustered.y, c=clustered.labels, s=0.05, cmap='hsv_r')
+plt.colorbar()
+
+
+docs_df = pd.DataFrame(data, columns=["Doc"])
+docs_df['Topic'] = cluster.labels_
+docs_df['Doc_ID'] = range(len(docs_df))
+docs_per_topic = docs_df.groupby(['Topic'], as_index = False).agg({'Doc': ' '.join})
+
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def c_tf_idf(documents, m, ngram_range=(1, 1)):
+    count = CountVectorizer(ngram_range=ngram_range, stop_words="english").fit(documents)
+    t = count.transform(documents).toarray()
+    w = t.sum(axis=1)
+    tf = np.divide(t.T, w)
+    sum_t = t.sum(axis=0)
+    idf = np.log(np.divide(m, sum_t)).reshape(-1, 1)
+    tf_idf = np.multiply(tf, idf)
+
+    return tf_idf, count
+
+
+tf_idf, count = c_tf_idf(docs_per_topic.Doc.values, m=len(data))
+
+
+
+def extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20):
+    words = count.get_feature_names()
+    labels = list(docs_per_topic.Topic)
+    tf_idf_transposed = tf_idf.T
+    indices = tf_idf_transposed.argsort()[:, -n:]
+    top_n_words = {label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1] for i, label in enumerate(labels)}
+    return top_n_words
+
+def extract_topic_sizes(df):
+    topic_sizes = (df.groupby(['Topic'])
+                     .Doc
+                     .count()
+                     .reset_index()
+                     .rename({"Topic": "Topic", "Doc": "Size"}, axis='columns')
+                     .sort_values("Size", ascending=False))
+    return topic_sizes
+
+top_n_words = extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20)
+topic_sizes = extract_topic_sizes(docs_df); topic_sizes.head(10)
+
+
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+for i in range(20):
+    # Calculate cosine similarity
+    similarities = cosine_similarity(tf_idf.T)
+    np.fill_diagonal(similarities, 0)
+
+    # Extract label to merge into and from where
+    topic_sizes = docs_df.groupby(['Topic']).count().sort_values("Doc", ascending=False).reset_index()
+    topic_to_merge = topic_sizes.iloc[-1].Topic
+    topic_to_merge_into = np.argmax(similarities[topic_to_merge + 1]) - 1
+
+    # Adjust topics
+    docs_df.loc[docs_df.Topic == topic_to_merge, "Topic"] = topic_to_merge_into
+    old_topics = docs_df.sort_values("Topic").Topic.unique()
+    map_topics = {old_topic: index - 1 for index, old_topic in enumerate(old_topics)}
+    docs_df.Topic = docs_df.Topic.map(map_topics)
+    docs_per_topic = docs_df.groupby(['Topic'], as_index = False).agg({'Doc': ' '.join})
+
+    # Calculate new topic words
+    m = len(data)
+    tf_idf, count = c_tf_idf(docs_per_topic.Doc.values, m)
+    top_n_words = extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20)
+
+topic_sizes = extract_topic_sizes(docs_df); topic_sizes.head(10)
