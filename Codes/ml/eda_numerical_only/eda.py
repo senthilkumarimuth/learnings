042cb7fa6cf062c3d0e237ceeac70be3074a3604
senthil.marimuthu <senthil.marimuthu@htcindia.com>
2022-03-15 14:46:42 +0530

    meidan value - duplicate implementation is removed

--- Codes/ml/eda_numerical_only/eda.py
+++ Codes/ml/eda_numerical_only/eda.py
@@ -6,20 +6,15 @@ import seaborn as sns
 # read the dataset
 df = pd.read_csv('export-33.csv')
 
-#mean,min,max,percentile,count,std
-result = df.describe([0.01, 0.05, 0.10, 0.90, 0.95,0.99])
+#mean,min,max,percentile,count,std, 0.50 is median
+result = df.describe([0.01, 0.05, 0.10, 0.50,0.90, 0.95,0.99])
 
-#median
-median_series = df.median(axis = 0)
-median_df = pd.DataFrame(median_series)
-median_df = median_df.transpose()
-median_df = median_df.rename(index = {0:'Median'})
 #null values count
 na_series = df.isnull().sum(axis = 0)
 na_df = pd.DataFrame(na_series)
 na_df = na_df.transpose()
 na_df = na_df.rename(index={0: 'Null values'})
-result = pd.concat([result, median_df, na_df])
+result = pd.concat([result, na_df])
 
 #histogram
 for column in df.columns:
